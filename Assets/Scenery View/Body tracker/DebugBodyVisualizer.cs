916b3995e902073c9f005adc1048e293cb8f904e
Aapo Nikkilä <aaponikkila@hotmail.com>
2016-10-22 21:39:52 +0300

    Replaced previously used example project files with new implementation. Refined scenery movement.

---
+++ Assets/Scenery View/Body tracker/DebugBodyVisualizer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Kinect = Windows.Kinect;
+
+public class DebugBodyVisualizer : MonoBehaviour
+{
+
+    public BodyTracker bodyTracker;
+
+    void OnDrawGizmos()
+    {
+        Dictionary<ulong, Kinect.Body> trackedIds = null;
+        if (bodyTracker != null)
+        {
+            trackedIds = bodyTracker.TrackedBodies();
+        }
+        if (trackedIds != null)
+        {
+            foreach (ulong id in trackedIds.Keys)
+            {
+                Kinect.Body body = trackedIds[id];
+                for (Kinect.JointType jt = Kinect.JointType.SpineBase; jt <= Kinect.JointType.ThumbRight; jt++)
+                {
+                    Kinect.Joint joint = body.Joints[jt];
+                    switch (joint.TrackingState)
+                    {
+                        case Kinect.TrackingState.Tracked:
+                            Gizmos.color = Color.green; break;
+                        case Kinect.TrackingState.Inferred:
+                            Gizmos.color = Color.yellow; break;
+                        case Kinect.TrackingState.NotTracked:
+                            Gizmos.color = Color.red; break;
+                    }
+                    Vector3 jointPosition = BodyTracker.CameraSpacePointToVector3(joint.Position);
+                    Gizmos.DrawSphere(jointPosition, 0.01f);
+                }
+
+                Gizmos.color = Color.blue;
+                Gizmos.DrawWireSphere(BodyTracker.BodyPosition(body), 0.1f);
+            }
+        }
+    }
+
+}
