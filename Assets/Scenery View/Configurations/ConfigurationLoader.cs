1ba02ac42514ee5499babc85a5596e7e91b4fb2d
Aapo Nikkilä <aaponikkila@hotmail.com>
2017-01-09 18:00:44 +0200

    Removed default Unity launcher. Moved display settings to Configurations.

---
+++ Assets/Scenery View/Configurations/ConfigurationLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConfigurationLoader : MonoBehaviour
+{
+
+    [Header("When running from the Unity editor, use this path to load configurations")]
+    public string configurationsPath;
+
+    void Start()
+    {
+        string[] args = System.Environment.GetCommandLineArgs();
+        if (args.Length >= 2)
+        {
+            // Load configurations from the path given as a command line argument
+            Configurations.Load(args[1]);
+        }
+        else
+        {
+            Configurations.Load(configurationsPath);
+        }
+    }
+}
